cdef55e3b12e0be71eab13bcc53a7690070eaf69
wesleybowman <wesley.bowman23@gmail.com>
2013-12-30 17:08:40 -0400

    made it better

--- sarah.py
+++ sarah.py
@@ -2,14 +2,18 @@ import numpy as np
 import pandas as pd
 
 #data = pd.read_excel('example_data.xls', 0, index_col=None, na_values=['NA'])
-data = pd.read_csv('example_data.csv',sep='\t')
+data = pd.read_csv('example_data.csv',sep='\t',index_col=0)
 
 print data
 print data.columns
 print data.index
 
-data = data.set_index(data.columns[0])
-print data.index
-print data.columns
+print data.loc[1]
+print data.loc[2]
 
-
+#data = data.set_index(data.columns[0])
+#print data.index
+#print data.columns
+#
+#
+#print data.index['1']

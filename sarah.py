af113d7f4b46830fc045377da05b4ccb5250ba64
wesleybowman <wesley.bowman23@gmail.com>
2014-01-01 13:55:49 -0400

    Added GUI for file input, and added averaged file for all the averages to be put into

--- sarah.py
+++ sarah.py
@@ -3,8 +3,12 @@ import numpy as np
 import pandas as pd
 import math
 import re
+from PySide.QtCore import *
+from PySide.QtGui import *
+import sys
+import os
 
-def sorted(trial,interval,aoi):
+def sorted(trial,interval,time,aoi,i,j):
     if aoi=='':
         for k,v in enumerate(interval):
                     try:
@@ -83,58 +87,90 @@ def sorted(trial,interval,aoi):
 
     return time
 
-data = pd.read_csv('example_data.csv',sep='\t',index_col=0)
-
-trials = data.index.unique()
-new = pd.DataFrame()
 
 
-for trial in trials:
-    if trial!=0:
-        start = data.loc[trial]['CURRENT_FIX_START']
-        end = data.loc[trial]['CURRENT_FIX_END']
+def fixData(file, saveAs):
+    data = pd.read_csv('example_data.csv',sep='\t',index_col=0)
 
-        areas = data.loc[trial]['CURRENT_FIX_INTEREST_AREAS']
-        areas = areas.values
-        aoi = []
-        for i in areas:
-            areanumbers = re.sub(r'[^\w]', '', i)
-            aoi.append(areanumbers)
+    trials = data.index.unique()
+    new = pd.DataFrame()
 
-        count = 0
 
-        for i, j in zip(start.values, end.values):
-            start = math.floor(i/100)*100
-            end = math.ceil(j/100)*100
-            interval = np.arange(start, end+1, 100)
+    for trial in trials:
+        if trial!=0:
+            start = data.loc[trial]['CURRENT_FIX_START']
+            end = data.loc[trial]['CURRENT_FIX_END']
 
-            time = []
-            time = sorted(trial,interval,aoi[count])
+            areas = data.loc[trial]['CURRENT_FIX_INTEREST_AREAS']
+            areas = areas.values
+            aoi = []
+            for i in areas:
+                areanumbers = re.sub(r'[^\w]', '', i)
+                aoi.append(areanumbers)
 
-            time = pd.DataFrame(time)
-            time = time.set_index(0)
-            new = pd.concat([new,time])
+            count = 0
 
-            count += 1
+            for i, j in zip(start.values, end.values):
+                start = math.floor(i/100)*100
+                end = math.ceil(j/100)*100
+                interval = np.arange(start, end+1, 100)
 
-new.columns = ['Time','% in 1','% in 2','% in 3','% in 4']
-new.index.name = 'Trial'
+                time = []
+                time = sorted(trial,interval,time,aoi[count],i,j)
 
-grouped = new.groupby([new.index,'Time'],as_index=False).sum()
+                time = pd.DataFrame(time)
+                time = time.set_index(0)
+                new = pd.concat([new,time])
 
-#Sorting
-grouped[['start', 'end']] = grouped['Time'].apply(lambda val: pd.Series(map(float, val.split('-'))))
-grouped.sort(['Trial','start', 'end'], inplace=True)
-grouped = grouped.set_index('Trial')
+                count += 1
 
-#Here we have a start and end column, which allows us to sort
-#Can keep that or get rid of it
-newData = grouped.drop(['start','end'],1)
+    new.columns = ['Time','% in 1','% in 2','% in 3','% in 4']
+    new.index.name = 'Trial'
 
-#Write all of the data to files
-new.to_csv('test.csv')
-grouped.to_csv('test2.csv')
-newData.to_csv('test3.csv')
+    grouped = new.groupby([new.index,'Time'],as_index=False).sum()
 
-averages = new.groupby(new.index).mean()
-averages.to_csv('average.csv')
+    grouped[['start', 'end']] = grouped['Time'].apply(lambda val: pd.Series(map(float, val.split('-'))))
+    grouped.sort(['Trial','start', 'end'], inplace=True)
+    grouped = grouped.set_index('Trial')
+
+    #Here we have a start and end column, which allows us to sort
+    #Can keep that or get rid of it
+    newData = grouped.drop(['start','end'],1)
+
+    #Write all of the data to files
+    #new.to_csv('test.csv')
+    #grouped.to_csv('test2.csv')
+    newData.to_csv(saveAs)
+
+    averages = new.groupby(new.index).mean()
+    #averages.to_csv('average.csv')
+
+    return averages
+
+
+app = QApplication(sys.argv)
+caption = 'Open Files'
+directory = './'
+#Do we need to delete old files?
+files = QFileDialog.getOpenFileNames(None, caption, directory)[0]
+
+averaged = pd.DataFrame()
+
+for file in files:
+    saveAs, ext=os.path.splitext(file)
+    saveAs = '{0}fixed.csv'.format(saveAs)
+    averageName = 'averaged.csv'
+
+    averages = fixData(file,saveAs)
+
+    saveAs = saveAs.split('/')
+    nameSpacer = pd.DataFrame({'Name':[saveAs[-1]]})
+
+    averaged = pd.concat([averaged,nameSpacer])
+    averaged = pd.concat([averaged,averages])
+
+    averaged.to_csv(averageName)
+
+
+
+
